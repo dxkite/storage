a2c9d5f2bbdaee39970e8536277261db6d5c0c92
dxkite <dxkite@163.com>
2020-08-09 08:56:16 +0800

    调整：项目READMD

--- const.go
+++ const.go
@@ -2,11 +2,11 @@ package storage
 
 const APP_NAME = "Storage"
 const BASE_PROTOCOL = "storage"
-const HOST_META = "meta"
+const HOST_META = "index"
 
-const EXT_META = ".meta"
+const EXT_META = ".storage"
 const EXT_UPLOADING = ".uploading"
 const EXT_DOWNLOADING = ".downloading"
 
-const META_INFO = "Storage Download Meta File"
-const META_NAME = "StorageMetaFile"
+const META_INFO = "Storage Download Index File"
+const META_NAME = "StorageFile"
